75f5da4c09a2a7b67ff2f3e3a823d3dc97981c35
Ancy-Anto <Ancy.Anto@ibsplc.com>
2025-07-11 09:27:38 +0530

    Car class added with constructor and method to display car info.

--- Sample3/featureClass.js
+++ Sample3/featureClass.js
@@ -1,1 +1,11 @@
 console.log("this is a sample web app");
+class car {
+  constructor(brand, model) {
+    this.brand = brand;
+    this.model = model;
+  }
+
+  displayInfo() {
+    console.log(`Car Brand: ${this.brand}, Model: ${this.model}`);
+  }
+}
